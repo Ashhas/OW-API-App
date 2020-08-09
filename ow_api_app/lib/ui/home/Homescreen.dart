8d1251f4e06869cc4b6be89ff959676e0456ab54
AJBruyning <aschwinbruyning@outlook.com>
2020-08-09 23:04:04 +0200

    + Created external widget for homebody

--- ow_api_app/lib/ui/home/Homescreen.dart
+++ ow_api_app/lib/ui/home/Homescreen.dart
@@ -1,9 +1,8 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:http/http.dart' as http;
 
 import 'package:ow_api_app/data/models/UserProfile.dart';
+import 'package:ow_api_app/ui/home/widget/ProfileDisplayWidget.dart';
 import 'package:provider_architecture/_viewmodel_provider.dart';
 
 import 'HomeScreenViewModel.dart';
@@ -18,7 +17,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   UserProfile currentUser;
-  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -36,74 +34,11 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget buildBody(viewModel) {
-    return Container(child: Center(child: profileDataWidget(viewModel)));
-  }
-
-  profileDataWidget(viewModel) {
     UserProfile currentUser = viewModel.currentUser;
-
-    return new Stack(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 120.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: 140,
-                height: 140,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: NetworkImage(currentUser.icon), fit: BoxFit.cover),
-                ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Text(
-                currentUser.name,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Card(
-                    color: Color.fromRGBO(48, 51, 66, 1),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        RankRatingWidget(
-                          rankRating: currentUser.rating.toString(),
-                          rankRole: "Tank",
-                          rankRatingIcon: currentUser.ratingIcon,
-                          rankRoleIcon: currentUser.ratingIcon,
-                        ),
-                        RankRatingWidget(
-                          rankRating: currentUser.ratings.damage.level.toString(),
-                          rankRole: "Damage",
-                          rankRatingIcon: currentUser.ratings.damage.rankIcon,
-                          rankRoleIcon: currentUser.ratings.damage.roleIcon,
-                        ),
-                        RankRatingWidget(
-                          rankRating: currentUser.ratings.support.level.toString(),
-                          rankRole: "Support",
-                          rankRatingIcon: currentUser.ratings.support.rankIcon,
-                          rankRoleIcon: currentUser.ratings.support.roleIcon,
-                        ),
-                      ],
-                    ),
-                  ))
-            ],
-          ),
-        )
-      ],
-    );
+    return Container(
+        child: Center(
+            child: ProfileDisplayWidget(
+      currentUser: currentUser,
+    )));
   }
 }
---
+++ ow_api_app/lib/ui/home/widget/ProfileDisplayWidget.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:ow_api_app/data/models/UserProfile.dart';
+
+import 'RankRatingWidget.dart';
+
+class ProfileDisplayWidget extends StatefulWidget {
+  final UserProfile currentUser;
+
+  const ProfileDisplayWidget({Key key, @required this.currentUser})
+      : super(key: key);
+
+  @override
+  _ProfileDisplayWidgetState createState() => _ProfileDisplayWidgetState();
+}
+
+class _ProfileDisplayWidgetState extends State<ProfileDisplayWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 120.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 140,
+                height: 140,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: NetworkImage(widget.currentUser.icon),
+                      fit: BoxFit.cover),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                widget.currentUser.name,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Card(
+                    color: Color.fromRGBO(48, 51, 66, 1),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        RankRatingWidget(
+                          rankRating: widget.currentUser.rating.toString(),
+                          rankRole: "Tank",
+                          rankRatingIcon: widget.currentUser.ratingIcon,
+                          rankRoleIcon: widget.currentUser.ratingIcon,
+                        ),
+                        RankRatingWidget(
+                          rankRating: widget.currentUser.ratings.damage.level
+                              .toString(),
+                          rankRole: "Damage",
+                          rankRatingIcon:
+                              widget.currentUser.ratings.damage.rankIcon,
+                          rankRoleIcon:
+                              widget.currentUser.ratings.damage.roleIcon,
+                        ),
+                        RankRatingWidget(
+                          rankRating: widget.currentUser.ratings.support.level
+                              .toString(),
+                          rankRole: "Support",
+                          rankRatingIcon:
+                              widget.currentUser.ratings.support.rankIcon,
+                          rankRoleIcon:
+                              widget.currentUser.ratings.support.roleIcon,
+                        ),
+                      ],
+                    ),
+                  ))
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
