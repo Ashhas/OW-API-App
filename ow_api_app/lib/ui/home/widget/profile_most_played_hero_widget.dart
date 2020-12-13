f3f09a60453c5dbf800acc6771aff4fce8744123
Aschwin B <aschwinbruyning@outlook.com>
2020-12-13 01:30:14 +0100

    + Added UI for private profiles

--- ow_api_app/lib/ui/home/widget/profile_most_played_hero_widget.dart
+++ ow_api_app/lib/ui/home/widget/profile_most_played_hero_widget.dart
@@ -15,170 +15,193 @@ class MostPlayedHeroesCard extends StatefulWidget {
 class _MostPlayedHeroesCardState extends State<MostPlayedHeroesCard> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Color.fromRGBO(24, 30, 36, 1.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            AspectRatio(
-              aspectRatio: 5.6 / 3,
-              child: Padding(
-                padding: EdgeInsets.only(left: 20, right: 20, top: 20),
-                child: Card(
-                  color: Color.fromRGBO(28, 42, 53, 1.0),
-                  elevation: 15,
-                  child: Column(
+    return widget.profileStatistics.private
+        ? Expanded(
+            child: Container(
+                color: Color.fromRGBO(24, 30, 36, 1.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      TitleView(
-                        titleTxt: "Most Played Heroes",
-                        subTxt: "See All",
+                      Icon(
+                        Icons.lock,
+                        color: Colors.white,
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 10, left: 10, bottom: 15, right: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                    ])))
+        : Expanded(
+            child: Container(
+              color: Color.fromRGBO(24, 30, 36, 1.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 5.6 / 3,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                      child: Card(
+                        color: Color.fromRGBO(28, 42, 53, 1.0),
+                        elevation: 15,
+                        child: Column(
                           children: [
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.blue,
-                                  ),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Card(
-                                  color: Color.fromRGBO(28, 39, 44, 1.0),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        "Tracer",
-                                        style: TextStyle(
-                                            fontFamily: "TitilliumWeb",
-                                            fontSize: 17,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                      Text(
-                                        "4 HRS",
-                                        style: TextStyle(
-                                            fontFamily: "TitilliumWeb",
-                                            fontSize: 13,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                      ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(8.0),
-                                          child: Image(
-                                              height: 70,
-                                              width: 70,
-                                              image: NetworkImage(widget
-                                                  .profileStatistics.icon))),
-                                    ],
-                                  ),
-                                ),
-                              ),
+                            TitleView(
+                              titleTxt: "Most Played Heroes",
+                              subTxt: "See All",
                             ),
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.blue,
-                                  ),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Card(
-                                  color: Color.fromRGBO(28, 39, 44, 1.0),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        "Tracer",
-                                        style: TextStyle(
-                                            fontFamily: "TitilliumWeb",
-                                            fontSize: 17,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w500),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  top: 10, left: 10, bottom: 15, right: 10),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors.blue,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
                                       ),
-                                      Text(
-                                        "4 HRS",
-                                        style: TextStyle(
-                                            fontFamily: "TitilliumWeb",
-                                            fontSize: 13,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w500),
+                                      child: Card(
+                                        color: Color.fromRGBO(28, 39, 44, 1.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              "Tracer",
+                                              style: TextStyle(
+                                                  fontFamily: "TitilliumWeb",
+                                                  fontSize: 17,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            Text(
+                                              "4 HRS",
+                                              style: TextStyle(
+                                                  fontFamily: "TitilliumWeb",
+                                                  fontSize: 13,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(8.0),
+                                                child: Image(
+                                                    height: 70,
+                                                    width: 70,
+                                                    image: NetworkImage(widget
+                                                        .profileStatistics
+                                                        .icon))),
+                                          ],
+                                        ),
                                       ),
-                                      ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(8.0),
-                                          child: Image(
-                                              height: 70,
-                                              width: 70,
-                                              image: NetworkImage(widget
-                                                  .profileStatistics.icon))),
-                                    ],
+                                    ),
                                   ),
-                                ),
+                                  Expanded(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors.blue,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                      child: Card(
+                                        color: Color.fromRGBO(28, 39, 44, 1.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              "Tracer",
+                                              style: TextStyle(
+                                                  fontFamily: "TitilliumWeb",
+                                                  fontSize: 17,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            Text(
+                                              "4 HRS",
+                                              style: TextStyle(
+                                                  fontFamily: "TitilliumWeb",
+                                                  fontSize: 13,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(8.0),
+                                                child: Image(
+                                                    height: 70,
+                                                    width: 70,
+                                                    image: NetworkImage(widget
+                                                        .profileStatistics
+                                                        .icon))),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors.blue,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                      child: Card(
+                                        color: Color.fromRGBO(28, 39, 44, 1.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              "Tracer",
+                                              style: TextStyle(
+                                                  fontFamily: "TitilliumWeb",
+                                                  fontSize: 17,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            Text(
+                                              "4 HRS",
+                                              style: TextStyle(
+                                                  fontFamily: "TitilliumWeb",
+                                                  fontSize: 13,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                            ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(8.0),
+                                                child: Image(
+                                                    height: 70,
+                                                    width: 70,
+                                                    image: NetworkImage(widget
+                                                        .profileStatistics
+                                                        .icon))),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.blue,
-                                  ),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Card(
-                                  color: Color.fromRGBO(28, 39, 44, 1.0),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        "Tracer",
-                                        style: TextStyle(
-                                            fontFamily: "TitilliumWeb",
-                                            fontSize: 17,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                      Text(
-                                        "4 HRS",
-                                        style: TextStyle(
-                                            fontFamily: "TitilliumWeb",
-                                            fontSize: 13,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                      ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(8.0),
-                                          child: Image(
-                                              height: 70,
-                                              width: 70,
-                                              image: NetworkImage(widget
-                                                  .profileStatistics.icon))),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
+                            )
                           ],
                         ),
-                      )
-                    ],
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ),
-          ],
-        ),
-      ), //last one
-    );
+            ), //last one
+          );
   }
 }
