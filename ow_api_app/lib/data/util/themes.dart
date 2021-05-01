52a11f467ca5e1934db29f801113ee1eb642e11c
Aschwin <aschwinbruyning@outlook.com>
2021-05-01 12:22:35 +0200

    + Changed UI Colors

--- ow_api_app/lib/data/util/themes.dart
+++ ow_api_app/lib/data/util/themes.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppThemes {
   static const Color _darkPrimaryColor = Color(0xFFF26513);
-  static const Color _darkCanvasColor = Color(0xFF303035);
+  static const Color _darkCanvasColor = Color(0xFF2D2F34);
   static const Color _darkCardColor = Color(0xFF2B2B31);
-  static const Color _darkTileColor = Color(0xFF333438);
-  static const Color _darkBackgroundColor = Color(0xFF2A2B2F);
+  static const Color _darkTileColor = Color(0xFF383B40);
+  static const Color _darkBackgroundColor = Color(0xFF27292D);
   static const Color _darkPrimaryTextColor = Color(0xFFF4F6FF);
 
   static ThemeData getDarkTheme() {
