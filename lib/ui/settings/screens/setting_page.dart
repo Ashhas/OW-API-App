a78110be4e2189a22f7faeba53f3b317acd41ac2
Aschwin B <aschwinbruyning@outlook.com>
2021-06-05 21:48:08 +0200

    + Added Setting Screen

---
+++ lib/ui/settings/screens/setting_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+  SettingPage();
+
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        width: double.infinity,
+        child: Center(
+          child: Text("Setting"),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      elevation: 1,
+      backgroundColor: Theme.of(context).canvasColor,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        color: Colors.black,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      title: Text(
+        "Setting",
+        style: Theme.of(context).primaryTextTheme.headline2,
+      ),
+    );
+  }
+}
