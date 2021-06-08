580e0f5ecc3b97029224ef2963829d1d85802beb
Aschwin B <aschwinbruyning@outlook.com>
2021-06-08 20:52:52 +0200

    ~ Seperated private profile widget

--- lib/ui/home/widgets/statistics_card.dart
+++ lib/ui/home/widgets/statistics_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ow_api_app/bloc/home/home_bloc.dart';
+import 'package:ow_api_app/ui/home/widgets/private_profile_widget.dart';
 import 'package:ow_api_app/ui/home/widgets/profile_most_played_hero_widget.dart';
 import 'package:ow_api_app/ui/home/widgets/profile_role_statistics_widget.dart';
 import 'package:ow_api_app/ui/home/widgets/rank_rating_widget.dart';
@@ -30,16 +31,7 @@ class _StatisticsCardState extends State<StatisticsCard> {
               return Padding(
                 padding: EdgeInsets.only(left: 20, right: 20),
                 child: state.profileStats.private
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Icon(
-                            Icons.lock,
-                            color: Colors.black,
-                          ),
-                        ],
-                      )
+                    ? PrivateProfileWidget()
                     : Column(
                         children: [
                           RankRatingWidget(),
