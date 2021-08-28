0d4dcea46fc87de91f7d9d7d54a3d18c3b9f3785
Aschwin B <aschwinbruyning@outlook.com>
2021-08-29 01:48:19 +0200

    + Settings now navigates based on state

--- lib/ui/settings/settings_screen.dart
+++ lib/ui/settings/settings_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ow_api_app/bloc/navigation/navigation_bar_bloc.dart';
 import 'package:ow_api_app/bloc/network_connection/network_connection_bloc.dart';
 import 'package:ow_api_app/ui/settings/screens/about_screen.dart';
 import 'package:ow_api_app/ui/settings/screens/add_profile_screen.dart';
@@ -38,36 +39,44 @@ class _SettingsScreenState extends State<SettingsScreen> {
       appBar: _buildAppBar(),
       backgroundColor: Theme.of(context).backgroundColor,
       body: BlocListener<NetworkConnectionBloc, NetworkConnectionState>(
-        listener: (context, networkState) {
-          if (networkState is NetworkConnectionUpdatedState) {
-            setState(() {
-              networkAvailable = true;
-            });
-          } else if (networkState is NoNetworkConnectionState) {
-            setState(() {
-              networkAvailable = false;
-            });
-          }
-        },
-        child: BlocBuilder<SettingsBloc, SettingsState>(
-          builder: (context, state) {
-            return SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildAvailableAccountsWidget(),
-                  Divider(height: 1, thickness: 1),
-                  _buildMainAccountTile(),
-                  Divider(height: 1, thickness: 1),
-                  _buildAboutTile(),
-                  SizedBox(height: 100),
-                ],
-              ),
-            );
+          listener: (context, networkState) {
+            if (networkState is NetworkConnectionUpdatedState) {
+              setState(() {
+                networkAvailable = true;
+              });
+            } else if (networkState is NoNetworkConnectionState) {
+              setState(() {
+                networkAvailable = false;
+              });
+            }
           },
-        ),
-      ),
+          child: BlocListener<SettingsBloc, SettingsState>(
+            listener: (context, settingsState) {
+              if (settingsState is ProfileChangedState) {
+                BlocProvider.of<NavigationBarBloc>(context).add(
+                  NavigateToScreen(selectedIndex: 0),
+                );
+              }
+            },
+            child: BlocBuilder<SettingsBloc, SettingsState>(
+              builder: (context, state) {
+                return SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildAvailableAccountsWidget(),
+                      Divider(height: 1, thickness: 1),
+                      _buildMainAccountTile(),
+                      Divider(height: 1, thickness: 1),
+                      _buildAboutTile(),
+                      SizedBox(height: 100),
+                    ],
+                  ),
+                );
+              },
+            ),
+          )),
     );
   }
 
